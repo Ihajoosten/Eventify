5a9ef0c18bb9eb03035736ebb8078128cd994943
Luc Joosten <lhajoosten@gmail.com>
2023-10-17 18:38:51 +0200

    Implemented integration tests for EFRepository<T>

---
+++ Eventify.Test/Configuration/TestDbContext.cs
@@ -0,0 +1,147 @@
+﻿using Eventify.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Eventify.Test.Configuration
+{
+    public class TestDbContext : DbContext
+    {
+        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options) { }
+
+        public DbSet<Registration> Registrations { get; set; }
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Sponsor> Sponsors { get; set; }
+        public DbSet<Speaker> Speakers { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<AttendeeFeedback> AttendeeFeedbacks { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public override DbSet<T> Set<T>() where T : class
+        {
+            return base.Set<T>();
+        }
+
+        public override EntityEntry<T> Entry<T>(T entity) where T : class
+        {
+            return base.Entry(entity);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddSerilog()));
+
+            optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Venue entity
+            modelBuilder.Entity<Venue>()
+                .HasMany(v => v.Events)
+                .WithOne(e => e.Venue)
+                .HasForeignKey(e => e.VenueId);
+
+            // Event entity
+            modelBuilder.Entity<Event>()
+                .HasOne(e => e.Venue)
+                .WithMany(v => v.Events)
+                .HasForeignKey(e => e.VenueId);
+
+            modelBuilder.Entity<Event>()
+                .HasOne(e => e.Organizer)
+                .WithMany(u => u.OrganizedEvents)
+                .HasForeignKey(e => e.OrganizerId);
+
+            modelBuilder.Entity<Event>()
+                .HasOne(e => e.Sponsor)
+                .WithMany(s => s.SponsoredEvents)
+                .HasForeignKey(e => e.SponsorId);
+
+            // Speaker entity
+            modelBuilder.Entity<Speaker>()
+                .HasMany(s => s.Sessions)
+                .WithOne(session => session.Speaker)
+                .HasForeignKey(session => session.SpeakerId);
+
+            // Session entity
+            modelBuilder.Entity<Session>()
+                .HasOne(session => session.Event)
+                .WithMany(e => e.Sessions)
+                .HasForeignKey(session => session.EventId);
+
+            // AttendeeFeedback entity
+            modelBuilder.Entity<AttendeeFeedback>()
+                .HasOne(feedback => feedback.Session)
+                .WithMany(session => session.Feedbacks)
+                .HasForeignKey(feedback => feedback.SessionId);
+
+            modelBuilder.Entity<AttendeeFeedback>()
+                .HasOne(feedback => feedback.User)
+                .WithMany(user => user.Feedbacks)
+                .HasForeignKey(feedback => feedback.UserId);
+
+            // User entity
+            modelBuilder.Entity<User>()
+                .HasMany(user => user.Feedbacks)
+                .WithOne(feedback => feedback.User)
+                .HasForeignKey(feedback => feedback.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(user => user.OrganizedEvents)
+                .WithOne(e => e.Organizer)
+                .HasForeignKey(e => e.OrganizerId);
+
+            // Add the Registration entity configuration
+            modelBuilder.Entity<Registration>()
+                .HasKey(r => r.Id);
+
+            // Relationships
+            modelBuilder.Entity<Registration>()
+                .HasOne(r => r.User)
+                .WithMany(u => u.Registrations)
+                .HasForeignKey(r => r.UserId);
+
+            modelBuilder.Entity<Registration>()
+                .HasOne(r => r.Event)
+                .WithMany(e => e.Registrations)
+                .HasForeignKey(r => r.EventId);
+
+            // Address owned property
+            modelBuilder.Entity<Venue>()
+                .OwnsOne(venue => venue.VenueAddress, address =>
+                {
+                    address.Property(a => a.Street).IsRequired().HasMaxLength(100);
+                    address.Property(a => a.City).IsRequired().HasMaxLength(50);
+                    address.Property(a => a.State).IsRequired().HasMaxLength(50);
+                    address.Property(a => a.ZipCode).IsRequired().HasMaxLength(20);
+                    address.Property(a => a.Country).IsRequired().HasMaxLength(50);
+                });
+
+            modelBuilder.Entity<User>()
+                .OwnsOne(user => user.UserAddress, address =>
+                {
+                    address.Property(a => a.Street).IsRequired().HasMaxLength(100);
+                    address.Property(a => a.City).IsRequired().HasMaxLength(50);
+                    address.Property(a => a.State).IsRequired().HasMaxLength(50);
+                    address.Property(a => a.ZipCode).IsRequired().HasMaxLength(20);
+                    address.Property(a => a.Country).IsRequired().HasMaxLength(50);
+                });
+
+            // Additional configurations...
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Eventify.Test/Domain/IRepositoryTests.cs
@@ -0,0 +1,119 @@
+﻿using Eventify.Domain.Entities;
+using Eventify.Domain.IRepositories.Base;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventify.Test.Domain
+{
+    public class IRepositoryTests
+    {
+        [Fact]
+        public async Task GetByIdAsync_Returns_Entity_When_Found()
+        {
+            // Arrange
+            var entityId = Guid.NewGuid();
+            var entity = new User { Id = entityId };
+            var repository = new Mock<IRepository<User>>();
+            repository.Setup(r => r.GetByIdAsync(entityId)).ReturnsAsync(entity);
+
+            // Act
+            var result = await repository.Object.GetByIdAsync(entityId);
+
+            // Assert
+            Assert.Equal(entity, result);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_Returns_Null_When_Entity_Not_Found()
+        {
+            // Arrange
+            var entityId = Guid.NewGuid();
+            var repository = new Mock<IRepository<User>>();
+            repository.Setup(r => r.GetByIdAsync(entityId)).ReturnsAsync((User)null);
+
+            // Act
+            var result = await repository.Object.GetByIdAsync(entityId);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_Returns_Entities_When_Found()
+        {
+            // Arrange
+            var entities = new List<User> { new User(), new User() };
+            var repository = new Mock<IRepository<User>>();
+            repository.Setup(r => r.GetAllAsync()).ReturnsAsync(entities);
+
+            // Act
+            var result = await repository.Object.GetAllAsync();
+
+            // Assert
+            Assert.Equal(entities, result);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_Returns_Null_When_No_Entities_Found()
+        {
+            // Arrange
+            var repository = new Mock<IRepository<User>>();
+            repository.Setup(r => r.GetAllAsync()).ReturnsAsync((IEnumerable<User>)null);
+
+            // Act
+            var result = await repository.Object.GetAllAsync();
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task AddAsync_Returns_Added_Entity()
+        {
+            // Arrange
+            var entity = new User();
+            var repository = new Mock<IRepository<User>>();
+            repository.Setup(r => r.AddAsync(entity)).ReturnsAsync(entity);
+
+            // Act
+            var result = await repository.Object.AddAsync(entity);
+
+            // Assert
+            Assert.Equal(entity, result);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_Returns_Updated_Entity()
+        {
+            // Arrange
+            var entity = new User();
+            var repository = new Mock<IRepository<User>>();
+            repository.Setup(r => r.UpdateAsync(entity)).ReturnsAsync(entity);
+
+            // Act
+            var result = await repository.Object.UpdateAsync(entity);
+
+            // Assert
+            Assert.Equal(entity, result);
+        }
+
+        [Fact]
+        public async Task RemoveAsync_Returns_Removed_Entity()
+        {
+            // Arrange
+            var entity = new User();
+            var repository = new Mock<IRepository<User>>();
+            repository.Setup(r => r.RemoveAsync(entity)).ReturnsAsync(entity);
+
+            // Act
+            var result = await repository.Object.RemoveAsync(entity);
+
+            // Assert
+            Assert.Equal(entity, result);
+        }
+    }
+}
