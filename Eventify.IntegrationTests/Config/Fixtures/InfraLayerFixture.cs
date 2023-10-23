4ae36bd5fa61dbc0bacac70646e589408dfd9b09
Luc Joosten <lhajoosten@gmail.com>
2023-10-23 13:13:27 +0200

    adjusted test db environment

--- Eventify.IntegrationTests/Config/Fixtures/InfraLayerFixture.cs
+++ Eventify.IntegrationTests/Config/Fixtures/InfraLayerFixture.cs
@@ -9,15 +9,28 @@
         {
             // Initialize the database context with the same connection string
             _options = new DbContextOptionsBuilder<TestDbContext>()
-                .UseNpgsql("Host=localhost;Port=5432;Database=eventify-infra-test;Username=postgres;Password=postgres;Integrated Security=true;Pooling=true;")
+                .UseNpgsql("Host=ep-soft-sunset-52153316.eu-central-1.aws.neon.tech;Port=5432;Database=eventify1-test;Username=Ihajoosten;Password=vaIM1win0Ucs;Integrated Security=true;")
                 .Options;
 
             Context = new TestDbContext(_options);
 
             // Ensure the database is created and apply migrations
             Context.Database.EnsureCreated();
+
+            // Clean Database
+            Context.Users.Clear();
+            Context.Venues.Clear();
+            Context.Events.Clear();
+            Context.Sponsors.Clear();
+            Context.Speakers.Clear();
+            Context.Sessions.Clear();
+            Context.Registrations.Clear();
+            Context.AttendeeFeedbacks.Clear();
+            Context.SaveChanges();
+
             // Migrate Database
             Context.Database.Migrate();
+
             // Seed the database with initial data
             SeedData.Initialize(Context);
         }
@@ -25,7 +38,7 @@
         public void Dispose()
         {
             // Clean up the database after all tests have run
-            Context.Database.EnsureDeleted();
+            //Context.Database.EnsureDeleted();
             Context.Dispose();
         }
     }
